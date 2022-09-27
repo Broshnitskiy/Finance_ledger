350ea3a236d7ae621b5e5e75f5c3a79c3f178605
Roman Broshnitskiy <romario777v8@gmail.com>
2022-09-27 21:27:15 +0300

    config form

--- src/components/contact/ContactSection.jsx
+++ src/components/contact/ContactSection.jsx
@@ -3,9 +3,41 @@ import imgContactWebp1x from '../../images/home/contact.webp';
 import imgContactWebp2x from '../../images/home/contact@2x.webp';
 import imgContactJpg1x from '../../images/home/contact.jpg';
 import imgContactJpg2x from '../../images/home/contact@2x.jpg';
-import { Section, Picture, Div } from './ContactSection.styled';
+import {
+  Section,
+  Picture,
+  Div,
+  Input,
+  ErrorWraper,
+  TextError,
+} from './ContactSection.styled';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { MdWarning } from 'react-icons/md';
 
 export const ContactSection = () => {
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .matches(
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, //eslint-disable-line
+        'Invalid e-mail',
+      )
+      .min(3, 'Must contain at least 3 characters!')
+      .max(254, 'Can not contain more than 254 characters!')
+      .required(`This is required field!`),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+    },
+    validationSchema,
+    onSubmit: values => {
+      formik.resetForm({ name: '', email: '' });
+    },
+  });
+
   return (
     <Section id="contact">
       <Picture>
@@ -20,13 +52,35 @@ export const ContactSection = () => {
 
       <Div>
         <h2>Request Callback</h2>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate,
-          ipsum dignissimos quae laudantium asperiores nam aliquid impedit harum
-          illum dolore explicabo ab dolores itaque rerum temporibus doloribus
-          iste maiores deleniti?
-        </p>
-        <button type="submit">Send</button>
+        <form onSubmit={formik.handleSubmit}>
+          <Input
+            type="text"
+            name="name"
+            autoComplete="off"
+            placeholder="Enter your name"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur('name')}
+            value={formik.values.name}
+          />
+
+          <Input
+            type="email"
+            name="email"
+            placeholder="Enter email *"
+            autoComplete="off"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur('email')}
+            value={formik.values.email}
+          />
+          {formik.errors.email && formik.touched.email && (
+            <ErrorWraper>
+              <MdWarning color="red" />
+              <TextError>{formik.errors.email}</TextError>
+            </ErrorWraper>
+          )}
+
+          <button type="submit">Send</button>
+        </form>
       </Div>
     </Section>
   );
--- src/components/contact/ContactSection.styled.jsx
+++ src/components/contact/ContactSection.styled.jsx
@@ -4,7 +4,6 @@ export const Section = styled.section`
   max-width: 480px;
   margin-left: auto;
   margin-right: auto;
-  padding-bottom: 25px;
 
   @media screen and (min-width: 768px) {
     max-width: 768px;
@@ -24,14 +23,24 @@ export const Div = styled.div`
   padding-left: 20px;
   padding-right: 20px;
   padding-top: 51px;
+  padding-bottom: 25px;
 
   h2 {
+    text-align: center;
     font-size: 32px;
     line-height: 48px;
     margin-bottom: 69px;
   }
 
+  form {
+    width: 280px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
   button {
+    display: block;
+    margin-top: 25px;
     font-size: 18px;
     line-height: 25px;
     width: 155px;
@@ -51,11 +60,19 @@ export const Div = styled.div`
     padding-left: 32px;
     padding-right: 32px;
     padding-top: 32px;
+    padding-bottom: 0;
 
     h2 {
       font-size: 40px;
       line-height: 54px;
       margin-bottom: 24px;
+      text-align: left;
+    }
+
+    form {
+      width: 336px;
+      margin-left: 0;
+      margin-right: 0;
     }
 
     button {
@@ -70,6 +87,10 @@ export const Div = styled.div`
 
     h2 {
       margin-bottom: 31px;
+    }
+
+    form {
+      width: 642px;
     }
   }
 `;
@@ -88,5 +109,44 @@ export const Picture = styled.picture`
         min-height: 460px;
       }
     }
+  }
+`;
+
+export const Input = styled.input`
+  display: block;
+  width: 100%;
+  height: 56px;
+  padding-left: 8px;
+  font-size: 16px;
+  line-height: 23px;
+  &:first-child {
+    margin-bottom: 16px;
+  }
+
+  background: #ffffff;
+  border: none;
+  border-radius: 5px;
+  ::placeholder {
+    color: #808080;
+  }
+
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+    line-height: 25px;
+  }
+`;
+
+export const ErrorWraper = styled.div`
+  display: flex;
+`;
+
+export const TextError = styled.span`
+  width: 280px;
+  word-break: break-all;
+  color: red;
+  font-size: 12px;
+
+  @media screen and (min-width: 768px) {
+    width: 240px;
   }
 `;
